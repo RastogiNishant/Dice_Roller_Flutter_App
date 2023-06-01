6e318b2cdffe851bc2296705874016416acf1289
NV Rastogi <rastoginishant@yahoo.in>
2023-06-01 10:36:45 +0530

    Custom Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,14 @@
+import "package:demo_app/gradient_container.dart";
 import "package:flutter/material.dart";
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 26, 2, 80),
-                Color.fromARGB(255, 45, 7, 98),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello World',
-              style: TextStyle(color: Colors.white, fontSize: 28),
-            ),
-          ),
-        ),
-      ),
+          body: GradientContainer(
+        Color.fromARGB(255, 26, 2, 80),
+        Color.fromARGB(255, 45, 7, 98),
+      )),
     ),
   );
 }
